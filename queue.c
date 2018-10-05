ca54d17c988b779ae7da2c51fd72e42b5fc86952
LiuJuiHung <ray9956666@gmai.com>
2018-10-05 22:19:31 +0800

    Implement  functions
    implement q_new()
    q_free()
    q_insert_head

--- queue.c
+++ queue.c
@@ -26,7 +26,13 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (!q)
+        q->head = NULL;
+    else {
+        q->head = NULL;
+        q->tail = NULL;
+        q->q_size = 0;
+    }
     return q;
 }
 
@@ -35,6 +41,13 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (!q)
+        return;
+    while (q->head) {
+        list_ele_t *tmp = q->head;
+        q->head = tmp->next;  // q->head = q->head->next
+        free(tmp);
+    }
     free(q);
 }
 
@@ -47,14 +60,28 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        return false;
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
-    return true;
+    if (!newh)
+        return false;
+    else {
+        newh->value = strdup(s);
+        if (q->q_size == 0) {
+            q->tail = newh;
+            newh->next = NULL;
+        } else {
+            newh->next = q->head;
+        }
+        q->head = newh;
+        q->q_size++;
+        return true;
+    }
+    return false;
 }
 
 
