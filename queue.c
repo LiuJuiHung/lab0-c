203efecde15ff0b837dc20085eaec38c796f0406
LiuJuiHung <ray9956666@gmai.com>
2018-10-06 10:54:11 +0800

    Implement functions
    implement q_size()

--- queue.c
+++ queue.c
@@ -138,7 +138,10 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (!q) {
+        return 0;
+    }
+    return q->q_size;
 }
 
 /*
